a624822dc19a25de10e05448ed76f63cf2a5cb72
Matt Smith <mattdsmith89@users.noreply.github.com>
2020-06-23 20:46:25 +0100

    Update Home.js

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -16,7 +16,7 @@ export class Home extends Component {
       <div>
         <h1>Home</h1>
         <p>
-          Hello, {this.props.name}. Welcome to CoinDay!
+          Hello, {this.props.name}. Welcome to Coin Day!
         </p>
         <Button color="secondary" size="sm" onClick={this.handleClick}>
           Not {this.props.name}?
